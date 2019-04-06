80eeadc2312578003eb9342894a1b9e7b3550e7d
CliffCrerar <cliff.crerar@gmail.com>
2019-04-06 22:10:41 +0200

    Commit for creation of clean branch for other branch setup.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /**
- * VSCODE ENVIRONEMENTS CONTROLLER
+ * VSCODE ENVIRONMENTS CONTROLLER
  *
  * @summary short description for the file
  * @author Cliff Crerar
  *
  * Created at     : 2019-04-06 21:13:28
- * Last modified  : 2019-04-06 21:13:48
+ * Last modified  : 2019-04-06 21:40:18
  */
 
+const fs = require('fs');
