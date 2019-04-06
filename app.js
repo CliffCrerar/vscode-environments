25fcd4dee965572a064abfef9b6a4daaf678c573
CliffCrerar <cliff.crerar@gmail.com>
2019-04-06 22:09:54 +0200

    Commit for creation of clean branch

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * VSCODE ENVIRONEMENTS CONTROLLER
+ *
+ * @summary short description for the file
+ * @author Cliff Crerar
+ *
+ * Created at     : 2019-04-06 21:13:28
+ * Last modified  : 2019-04-06 21:13:48
+ */
+
