73171ff8956633ac54a6ba1c887d6083a4ea32f4
CliffCrerar <cliff.crerar@gmail.com>
2019-04-09 05:56:33 +0200

    Doing some lekke things my bru

---
+++ com/install.sh
@@ -0,0 +1,4 @@
+# code --install-extensions $1.vsix
+read -p 'Are you a user': user
+ehco 'Command line installing'
+echo 'Installing: $1.vsix'
---
+++ com/test.sh
@@ -0,0 +1,7 @@
+
+
+read -p 'Are you a user': user
+
+echo user
+
+echo $1 $2
